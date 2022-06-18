4630704ecb43942093dc6fcd4a801eac04bde722
aivazashvilitekla <tekla.aivazashvili@gau.edu.ge>
2022-06-18 12:52:16 +0400

    carousel

--- src/app/shell/main/carousel/carousel.component.ts
+++ src/app/shell/main/carousel/carousel.component.ts
@@ -10,27 +10,78 @@ import {
 export class CarouselComponent implements OnInit {
   faArrowCircleLeft = faArrowCircleLeft
   faArrowCircleRight = faArrowCircleRight
-  slides: string[] = [
-    '../../../../assets/images/authors/galaktion.jpg',
-    '../../../../assets/images/authors/terenti.jpg',
-    '../../../../assets/images/authors/Prince_Nikoloz_Baratashvili.jpg',
-  ];
-  i = 0;
+  // slides: string[] = [
+  //   '../../../../assets/images/Slider1.jpg',
+  //   '../../../../assets/images/Slider2.jpg',
+  //   '../../../../assets/images/Slider3.jpg',
+  //   '../../../../assets/images/Slider4.jpg',
+  // ];
+  // i = 0;
 
-  getSlide() {
-    return this.slides[this.i];
-  }
+  // getSlide() {
+  //   return this.slides[this.i];
+  // }
 
-  getPrev() {
-    this.i = this.i === 0 ? 2 : this.i - 1;
-  }
-  //edit here
-  getNext() {
-    this.i = this.i === 2 ? 0 : this.i + 1;
-    console.log(this.i)
-  }
+  // getPrev() {
+  //   this.i = this.i === 0 ? 3 : this.i - 1;
+  // }
+  // //edit here
+  // getNext() {
+  //   this.i = this.i === 3 ? 0 : this.i + 1;
+  //   console.log(this.i)
+  // }
   constructor() {}
   ngOnInit(): void {
-    console.log(this.i)
+    // console.log(this.i)
+    this.showSlides(this.slideIndex);
   }
+
+slideIndex = 1;
+
+
+// Start autoplaying automatically
+  autoplayInterval = setInterval(() => {
+    console.log('bla')
+    document.getElementById("next")?.click()
+}, 2000); // Do this every 1 second, increase this!
+
+// Stop function added to button
+// stopAutoplay() {
+
+//   // Stop the autoplay
+//   clearInterval(this.autoplayInterval);
+
+// }
+// Next/previous controls
+plusSlides(n:any) {
+  this.showSlides(this.slideIndex += n);
+}
+
+// Thumbnail image controls
+currentSlide(n:any) {
+  this.showSlides(this.slideIndex = n);
+}
+
+showSlides(n:any) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    this.slideIndex = 1
+  }
+  if (n < 1) {
+    this.slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    (slides[i] as HTMLElement).style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  (slides[this.slideIndex - 1] as HTMLElement).style.display = "block";
+  dots[this.slideIndex - 1].className += " active";
+}
+
+
 }
